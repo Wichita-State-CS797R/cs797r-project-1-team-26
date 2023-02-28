cf72188047bc4e34dc9b68290a69856f3333abb0
h765f873 <124842450+h765f873@users.noreply.github.com>
2023-02-27 23:21:53 -0600

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Project1.Themes;
+
+namespace Project1;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+    void DarkMode(object Passer, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+
+            mergedDictionaries.Add(new DarkMode());
+
+        }
+    }
+
+    void LightMode(object Passer, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+
+            mergedDictionaries.Add(new LightMode());
+
+        }
+    }
+
+    void MarsMode(object Passer, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+
+            mergedDictionaries.Add(new MarsMode());
+
+        }
+    }
+    void PinkMode(object Passer, EventArgs e)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+
+            mergedDictionaries.Add(new PinkMode());
+
+        }
+    }
+}
